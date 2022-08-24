7197ea28defffcecaae4e90342a3372446987a22
Muhammad Akhta <akhtacaesar@gmail.com>
2022-08-24 14:25:46 +0700

    Added Part of Auth

--- progression/urls.py
+++ progression/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
 from . import views
+from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="progression"),
+    path('authentication/', include('authentication.urls')),
 ]

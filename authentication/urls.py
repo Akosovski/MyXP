438615a2d85e3a4161654a36198a501ba9e41518
Muhammad Akhta <akhtacaesar@gmail.com>
2022-08-24 15:05:06 +0700

    Deleted Register Feature

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, UsernameValidationView, LogoutView
+from .views import LoginView, LogoutView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('login', LoginView.as_view(), name="login"),
     path('logout', LogoutView.as_view(), name="logout"),
-    path('register', RegistrationView.as_view(), name="register"),
-    path('username-validation', csrf_exempt(UsernameValidationView.as_view()), 
-        name="username-validation"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -9,42 +9,6 @@ from django.core.mail import EmailMessage
 from django.contrib import auth
 from django.contrib.auth.models import Permission
 
-class UsernameValidationView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-        username = data['username']
-        if not str(username).isalnum():
-            return JsonResponse({'username_error':'Nama tidak boleh ada simbol!'},status=400)
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({'username_error':'Nama sudah ada!'},status=409)
-        return JsonResponse({'username_valid':True})
-
-class RegistrationView(View):
-    def get(self, request):
-        return render(request, 'authentication/register.html')
-
-    def post(self, request):
-       username = request.POST['username']
-       email = request.POST['email']
-       password = request.POST['password']
-       context={
-        'fieldValues':request.POST
-       }
-       if not User.objects.filter(username=username).exists():
-            if not User.objects.filter(email=email).exists():
-                if len(password)<6:
-                    messages.error(request, 'Kata Sandi terlalu pendek!')
-                    return render(request, 'authentication/register.html', context)
-
-                user = User.objects.create_user(username=username,email=email,)
-                user.set_password(password)
-                user.save()
-                                
-                messages.success(request, 'Akun Berhasil Dibuat!')
-                return render(request, 'authentication/register.html')
-
-       return render(request, 'authentication/register.html')
-
 class LoginView(View):
     def get(self, request):
         return render(request, 'authentication/login.html')

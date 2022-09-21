24c2e9a2a5f0ac0516da3ea97630f39108192607
Muhammad Akhta <akhtacaesar@gmail.com>
2022-09-21 14:22:48 +0700

    Added Quick Insert Button for XP

---
+++ static/js/quicker.js
@@ -0,0 +1,20 @@
+document.forms['add-activity']['xp'].value = 0;
+
+function add25() {
+    var xp = document.forms['add-activity']['xp'].value;
+    var intxp =  parseInt(xp);
+    var total = intxp + 25;
+    document.forms['add-activity']['xp'].value = total;
+}
+
+function add50() {
+
+}
+
+function add75() {
+
+}
+
+function add100() {
+
+}

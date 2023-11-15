b6395b8fadef8ef8a3e07dc5710361bb230eb4a1
Muhammad Akhta <akhtacaesar@gmail.com>
2023-11-15 21:40:50 +0700

    Added Static URL

--- myxp/urls.py
+++ myxp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('progression.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

76e3c5e5d2af94ea8e5fcafa5396f257e4fddfe3
Muhammad Akhta <akhtacaesar@gmail.com>
2023-11-15 20:51:32 +0700

    Static Fixes

--- myxp/settings.py
+++ myxp/settings.py
@@ -126,7 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'myxp/static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Static for Production
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

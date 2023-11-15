443669f6e773936fa61aa363dec980875098230d
Muhammad Akhta <akhtacaesar@gmail.com>
2023-11-15 21:46:57 +0700

    Collect Static

--- myxp/settings.py
+++ myxp/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR /'static']
+STATIC_ROOT = "https://myxp-production.up.railway.app/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

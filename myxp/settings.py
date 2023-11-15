c26028b12ef144f847a6dd0de3ab78cd99dc3fc4
Muhammad Akhta <akhtacaesar@gmail.com>
2023-11-15 21:23:45 +0700

    Revert Whitenoise

--- myxp/settings.py
+++ myxp/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'progression',
     'authentication'
 ]
@@ -46,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myxp.urls'
@@ -131,8 +129,6 @@ STATICFILES_DIRS = (
 
 # Static for Production
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

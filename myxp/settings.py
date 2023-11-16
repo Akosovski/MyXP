05eb894aaa12de17ae76bd86ce0ad42c084b3630
Muhammad Akhta <akhtacaesar@gmail.com>
2023-11-16 20:30:02 +0700

    Database Link Fix

--- myxp/settings.py
+++ myxp/settings.py
@@ -74,19 +74,19 @@ CSRF_TRUSTED_ORIGINS=['https://myxp-production.up.railway.app']
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': os.environ.get('DB_ENGINE'),
-       'NAME': os.environ.get('DB_NAME'),
-       'USER': os.environ.get('DB_USER'),
-       'PASSWORD': os.environ.get('DB_PASSWORD'),
-       'HOST': os.environ.get('DB_HOST'),
-   }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.config(default=os.environ.get('DB_URL'), conn_max_age=1800)
+#    'default': {
+#        'ENGINE': os.environ.get('DB_ENGINE'),
+#        'NAME': os.environ.get('DB_NAME'),
+#        'USER': os.environ.get('DB_USER'),
+#        'PASSWORD': os.environ.get('DB_PASSWORD'),
+#        'HOST': os.environ.get('DB_HOST'),
+#    }
 # }
+
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get('DB_URL'), conn_max_age=1800)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
